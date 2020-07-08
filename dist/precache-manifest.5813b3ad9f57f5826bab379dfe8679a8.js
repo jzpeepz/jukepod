8f1619d5e946b1f47d45171035198b4d46451de5
Jonathan Peoples <jonathan.peoples@gmail.com>
2020-07-07 21:06:29 -0500

    Adding dist folder

---
+++ dist/precache-manifest.5813b3ad9f57f5826bab379dfe8679a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ed3405824b0dafacda",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "d236ac25abfafc7029e5e11740346914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25585a7b2fca0884e070",
+    "url": "/js/about.4bc9425a.js"
+  },
+  {
+    "revision": "78ed3405824b0dafacda",
+    "url": "/js/app.3c6c430b.js"
+  },
+  {
+    "revision": "ffdfe53d328da087923f",
+    "url": "/js/chunk-vendors.bce1c6a2.js"
+  },
+  {
+    "revision": "a54e4a7e38103fedecd0815c274c6482",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
